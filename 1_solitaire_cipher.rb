75bbc91929ad930dacb3bfa123959efa01eac8f6
TheBigL- <TheBigL-@users.noreply.github.com>
2016-07-17 12:40:14 -0500

    Update 1_solitaire_cipher.rb
    initial keystream generator created

--- 1_solitaire_cipher.rb
+++ 1_solitaire_cipher.rb
@@ -9,5 +9,20 @@
 # 6. Repeat 2 - 5 for the rest of the keys
 
 def solitaire_keystream(length)
-    deck = (1..54).to_a
+    deck = (1..52).to_a << "A" << "B"
+    keystream = []
+    wrap = Proc.new {|pos| pos > 53 ? pos - 53 : pos}
+    
+    until keystream.length == length do
+        deck.insert(wrap[deck.index("A") + 1],deck.delete_at(deck.index("A")))
+        deck.insert(wrap[deck.index("B") + 2],deck.delete_at(deck.index("B")))
+        bottom_J = [deck.index("A"),deck.index("B")].max + 1
+        bottom_slice = bottom_J > 53 ? nil : deck.slice!(bottom_J..-1)
+        top_J = [deck.index("A"),deck.index("B")].min - 1
+        deck += deck.slice!(0..top_J) unless top_J < 0
+        deck.insert(0,*bottom_slice)
+        deck.insert(-2,*deck.slice!(0..(deck[-1]-1)))
+        keystream.push(deck[0].is_a?(Fixnum) ? deck[deck[0]] : deck[53])
+    end
+    keystream
 end
